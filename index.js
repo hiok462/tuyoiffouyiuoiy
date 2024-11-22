28b32a35b528e3804602aa2b18c9ab7833fbbd5e
hiok462 <141525603+hiok462@users.noreply.github.com>
2024-11-22 22:36:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+app.listen(3000, () => {
+  console.log('server started');
+});
+const Discord = require("discord.js");
+const { token, prefix } = require("./config.json");
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+  ],
+});
+const { readdirSync } = require("node:fs");
+readdirSync("./handlers").forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
+});
+module.exports.Client = client;
+
+client.login(token);
